44f35e87c414844a1e82e7719e6fada40849ff17
Constantinos Kontozis (k22038792) <constantinos.kontozis@kcl.ac.uk>
2023-12-08 02:36:32 +0000

    Complete forms, views and templates for tests to run

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+"""Forms of the project."""
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput()
+        }
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) > 35:
+            raise forms.ValidationError("Ensure this value has at most 35 characters.")
+        return name
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data['quantity']
+        if quantity < 0 or quantity > 50:
+            raise forms.ValidationError("Quantity must be between 0 and 50.")
+        return quantity
